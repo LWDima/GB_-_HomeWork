2ccec98dcd83223a2e9a439d35212a94fa415df6
LWDima <volovichenko2012@yandex.ru>
2022-09-21 14:37:53 +0300

    make first HomeWork

---
+++ HW_Lesson1/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+using static System.Console;
+
+Clear();
+Write("Введите первое число ");
+int num1 = Convert.ToInt32(ReadLine());
+Write("Введите второе число число ");
+int num2 = Convert.ToInt32(ReadLine());
+
+
+
+if (num1 > num2)
+{
+    Write($"Первое число {num1} больше чем второе {num2}");
+}
+if (num2 > num1)
+{
+    Write($"Второе число {num2} больше чем первое {num1}");
+}
+if (num2 == num1)
+{
+    Write("Числа равны");
+}
---
+++ HW_Lesson1/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+using static System.Console;
+
+Clear();
+Write("Введите первое число ");
+int num1 = Convert.ToInt32(ReadLine());
+Write("Введите второе число число ");
+int num2 = Convert.ToInt32(ReadLine());
+Write("Введите третье число ");
+int num3 = Convert.ToInt32(ReadLine());
+
+int max = num1;
+
+if (num2 > max)
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+
+Write($"Максимальное число {max}");
