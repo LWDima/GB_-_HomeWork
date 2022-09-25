c120c46209f74271d25e5a766f03c5b2714c3677
LWDima <volovichenko2012@yandex.ru>
2022-09-25 13:20:23 +0300

    Make lesson 2 Home work

---
+++ HW_Lesson2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+using static System.Console;
+
+Clear();
+Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(ReadLine());
+
+if (num < 100)
+{
+    WriteLine("Третьей цифры нет");
+}
+else
+{
+    int result = num / 100 % 10;
+    WriteLine($" третья цифра числа {result}");
+}
