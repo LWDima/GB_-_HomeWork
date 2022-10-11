03d30a3ca2728d3b87f3210497426722290d9d50
LWDima <volovichenko2012@yandex.ru>
2022-10-11 19:25:38 +0300

    make HW 6

---
+++ HW_Lesson6/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+Clear();
+WriteLine("Введите положительные и отрицательные числа, через пробел: ");
+string[] array = ReadLine()!.Split(/*new char[] { ' ', ',' },*/ " ", StringSplitOptions.RemoveEmptyEntries);
+int SumArr = countNumbers(array);
+WriteLine(SumArr);
+
+
+int countNumbers(string[] inArray)
+{
+    int numbers = 0;
+    int[] ConvArray = new int[inArray.Length];
+
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        ConvArray[i] = Convert.ToInt32(inArray[i]);
+    }
+
+    for (int i = 0; i < ConvArray.Length; i++)
+    {
+        if (ConvArray[i] > 0) numbers++;
+    }
+    return numbers;
+}
+
+
+
---
+++ HW_Lesson6/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using static System.Console;
+Clear();
+WriteLine("Введите значения b1, k1, b2 и k2, через пробел: ");
+string[] array = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+double[] num = ConvertString(array);
+double x = (num[0] + num[2]) / (num[1] + num[3]);
+double y = num[1] * x + num[0];
+WriteLine($"Точка пересечения 2х прямых({x:f2}; {y:f2})");
+
+
+double[] ConvertString(string[] input)
+{
+    double[] result = new double[input.Length];
+    for (int i = 0; i < input.Length; i++)
+    {
+        result[i] = Convert.ToInt32(input[i]);
+    }
+    return result;
+}
+
+
+
