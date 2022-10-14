73e2dc833859f60f65ef8cd5ecb918899b98741a
LWDima <volovichenko2012@yandex.ru>
2022-10-14 15:15:39 +0300

    make HW9

---
+++ HW_Lesson9/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значения N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5. -> "5, 4, 3, 2, 1"
+// N = 8. -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+int count = 1;
+numsN(number, count);
+
+
+void numsN(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        numsN(n, count + 1);
+        Write(count + " ");
+    }
+}
---
+++ HW_Lesson9/Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+using static System.Console;
+Clear();
+Write("Введите положительное число M: ");
+int m = int.Parse(ReadLine()!);
+
+Write("Введите положительное число N: ");
+int n = int.Parse(ReadLine()!);
+
+FunAkker(m, n);
+
+
+// вызов функции Аккермана
+void FunAkker(int m, int n)
+{
+    Write(Akker(m, n));
+}
+
+// функция Аккермана
+int Akker(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akker(m - 1, 1);
+    }
+    else
+    {
+        return (Akker(m - 1, Akker(m, n - 1)));
+    }
+}
